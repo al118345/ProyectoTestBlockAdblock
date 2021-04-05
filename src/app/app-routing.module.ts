2a3e093191e70797d40e74d5abbcd797ee305a3f
ruben perez ibañez <rubenpeib@gmail.com>
2021-04-05 19:53:28 +0200

    Inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdblockActivoComponentComponent } from './adblock-activo-component/adblock-activo-component.component';
+import {AppComponent} from './app.component';
 
 
 const routes: Routes = [
-  { path: 'adblock_/:route', pathMatch: 'prefix', component: AdblockActivoComponentComponent }
-
+  { path: 'adblock_/:route', pathMatch: 'prefix', component: AdblockActivoComponentComponent },
+  { path: 'adblock_', component: AdblockActivoComponentComponent },
+  { path: '**', component: AppComponent },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
